3dd62691db538aec11de7bc42e55af46e6f8fa4c
Artur Feliks <arturfeliks4@gmail.com>
2023-09-23 15:40:44 +0200

    currencies table added

---
+++ src/currencies.js
@@ -0,0 +1,7 @@
+const currencies = [
+    { id: 1, short: "EUR", name: "Euro", rate: 4.6967 },
+    { id: 2, short: "GBP", name: "Funt brytyjski", rate: 5.3213 },
+    { id: 3, short: "USD", name: "Dolar amerykański", rate: 4.4508 },
+  ];
+
+  export { currencies };
