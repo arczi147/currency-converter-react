cccbafe0f9e63958cbc141423d36c9f62bfd45f3
Artur Feliks <arturfeliks4@gmail.com>
2023-09-25 16:03:40 +0200

    'Result' component shortened

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,14 +1,12 @@
-const Result = ({ result }) => {
-    return (
-        <p>
-            {result && (
-                <>
-                    {result.sourceAmount.toFixed(2)} {result.currency} =&nbsp;
-                    {result.targetAmount.toFixed(2)} PLN
-                </>
-            )}
-        </p>
-    );
-};
+const Result = ({ result }) => (
+    <p>
+        {result && (
+            <>
+                {result.sourceAmount.toFixed(2)} {result.currency} =&nbsp;
+                {result.targetAmount.toFixed(2)} PLN
+            </>
+        )}
+    </p>
+);
 
 export default Result;
