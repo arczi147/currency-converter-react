5ee5cf0720fd058ee2ab55d3df552e2093f72909
Artur Feliks <arturfeliks4@gmail.com>
2023-10-01 15:42:23 +0200

    names of const and background corrected

--- src/Form/Clock/useCurrentDate.js
+++ src/Form/Clock/useCurrentDate.js
@@ -4,12 +4,12 @@ const useCurrentDate = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        const IntervalId = setInterval(() => {
+        const intervalId = setInterval(() => {
             setDate(new Date());
         }, 1000);
 
         return () => {
-            clearInterval(IntervalId);
+            clearInterval(intervalId);
         };
     }, []);
 
