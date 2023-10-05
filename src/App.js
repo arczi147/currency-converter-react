56804765b7b53688ba6656c7de591af7da6ef502
Artur Feliks <arturfeliks4@gmail.com>
2023-10-05 15:18:32 +0200

    Footer modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Form from "./Form";
-import Footer from "./Footer";
 import Container from "./Container";
 import { useCurrentDate } from "./Form/Clock/useCurrentDate";
 
 function App() {
 
-const date = useCurrentDate();
+  const date = useCurrentDate();
 
   return (
     <Container>
-      <Form date={date}/>
-      <Footer />
+      <Form date={date} />
     </Container>
   )
 }
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,7 +2,7 @@ import { Content } from "./styled";
 
 const Footer = () => (
     <Content>
-        *Przelicznik kursu walut na dzień 24.02.2023 godz. 20:52 w Santander Bank.
+        *Przelicznik kursu walut jest aktualizowany na bieżąco.
     </Content>
 );
 
